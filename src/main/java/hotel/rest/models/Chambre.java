101510cd13f0d0f8851a4ee88245bbaed81a711e
Miguel-Delhelle <miguel.delhelle@gmail.com>
2025-01-09 18:42:08 +0100

    Data et relation bien défini

--- src/main/java/hotel/rest/models/Chambre.java
+++ src/main/java/hotel/rest/models/Chambre.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Transient;
 
 @Entity
 public class Chambre {
@@ -24,8 +22,10 @@ public class Chambre {
 	private double prix;
 	private TypeChambre typeChambre;
 	
-	@ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "hotel_id")
+	//@ManyToOne(optional = false, fetch = FetchType.LAZY)
+    //@JoinColumn(name = "hotel_id")
+	
+	@Transient
 	private Hotel hotel;
 	
 	private List <LocalDate> dateReservee = new ArrayList <LocalDate>();
